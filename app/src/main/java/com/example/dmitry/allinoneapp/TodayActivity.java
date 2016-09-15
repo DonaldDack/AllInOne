e1e4e9e3d8ad683469b014cb5c635185a8f886ac
DonaldDack <dyadichko-mitya@yandex.ru>
2016-09-15 15:20:55 +0300

    some changes

--- app/src/main/java/com/example/dmitry/allinoneapp/TodayActivity.java
+++ app/src/main/java/com/example/dmitry/allinoneapp/TodayActivity.java
@@ -2,6 +2,12 @@ package com.example.dmitry.allinoneapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class TodayActivity extends AppCompatActivity {
 
@@ -9,5 +15,11 @@ public class TodayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_today);
+
+        ((TextView) findViewById(R.id.text_clock)).setText((
+                new SimpleDateFormat("HH:HH").format(Calendar.getInstance().getTime())));
+
+        ((TextView) findViewById(R.id.text_clock)).setText((
+                new SimpleDateFormat("HH:HH").format(Calendar.getInstance().getTime())));
     }
 }
